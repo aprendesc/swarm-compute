88681f6adc8344bb519cb1fb20eae7f6b7024928
APC <47151810+aprendesc@users.noreply.github.com>
2025-08-21 12:43:59 +0200

    updates in selector

--- swarmcompute/configs/selector_config.py
+++ swarmcompute/configs/selector_config.py
@@ -1,66 +1,25 @@
 
 apps = {
-    '1': 'launch_master',
-    '2':'launch_security_node',
-    '3': 'cal_security_node',
+    '1':'launch_master_server',
+
         }
-print("""
-{
-    '1': 'launch_master',
-    '2':'launch_security_node',
-    '3': 'cal_security_node',
-        }
-""")
+print(str(apps))
 sel_n = input('Select app config: ')
 sel_app = apps[sel_n]
 
-if sel_app == 'launch_master':
+if sel_app == 'launch_master_server':
     password = input('Set a password for the master:')
+    exposed = input('Run exposed?(y/n)') == 'y'
+    master_address = 'tcp://localhost:5005' if not exposed else 'tcp://95.18.166.44:5005'
     config = {
         # NANO NET
         'mode': 'master',
-        'master_address': 'tcp://localhost:5005',
-        'password': password,
-        'node_name': 'security_node',
-        'node_method': lambda a, b: a + b,
-        'address_node': 'test_node',
-        'payload': {'a': 1, 'b': 2},
-        'delay': 1,
-    }
-
-elif sel_app == 'launch_security_node':
-    password = input('Set a password for the security node.')
-    env_file_path = input('set .env file path:')
-    if input('Use localhost?(y/n):') == 'y':
-        master_address = 'tcp://localhost:5005'
-    else:
-        master_address = 'tcp://95.18.166.44:5005'
-    def aux_method(dummy_input):
-        from dotenv import dotenv_values
-        env_vars = dotenv_values(env_file_path)
-        return env_vars
-    config = {
-        # NANO NET
-        'mode': 'node',
         'master_address': master_address,
         'password': password,
-        'node_name': 'security_node',
-        'node_method': aux_method,
+        'node_name': None,
+        'node_method': None,
         'address_node': None,
         'payload': None,
-        'delay': 1,
+        'delay': None,
     }
 
-elif sel_app == 'cal_security_node':
-    password = input('Set a password for access.')
-    config = {
-        # NANO NET
-        'mode': 'client',
-        'master_address': 'tcp://95.18.166.44:5005',
-        'password': password,
-        'node_name': 'security_node',
-        'node_method': None,
-        'address_node': 'security_node',
-        'payload': None,
-        'delay': 1,
-    }
