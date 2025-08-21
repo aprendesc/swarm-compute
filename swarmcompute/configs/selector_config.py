0d3821eac64f8228fb6882f74eaf30408c6cb01b
APC <47151810+aprendesc@users.noreply.github.com>
2025-08-21 12:51:52 +0200

    updates in selector

--- swarmcompute/configs/selector_config.py
+++ swarmcompute/configs/selector_config.py
@@ -8,12 +8,10 @@ sel_app = apps[sel_n]
 
 if sel_app == 'launch_master_server':
     password = input('Set a password for the master:')
-    exposed = input('Run exposed?(y/n)') == 'y'
-    master_address = 'tcp://localhost:5005' if not exposed else 'tcp://95.18.166.44:5005'
     config = {
         # NANO NET
         'mode': 'master',
-        'master_address': master_address,
+        'master_address': 'tcp://localhost:5005',
         'password': password,
         'node_name': None,
         'node_method': None,
